4bda11e810b0ae1e109afc6038c18fabc1bac1df
NunoLava1998 <nunolava1998@gmail.com>
2017-02-24 17:25:47 +0000

    Adding first commit

---
+++ kernel.c
@@ -0,0 +1,7 @@
+
+#include <prototypes.h>
+
+void kmain(void) {
+	puts("Running in the nineties!", 0, 15);
+	for(;;);
+}
---
+++ prototypes.h
@@ -0,0 +1,13 @@
+
+#ifndef _PROTOTYPES_H
+#define _PROTOTYPES_H
+
+extern int x;
+extern int y;
+extern unsigned short int* vptr;
+extern void update_vptr(int, int);
+extern void update_coord(unsigned short int*);
+extern void putc(char, unsigned char, unsigned char);
+extern void puts(const char*, unsigned char, unsigned char);
+
+#endif // _PROTOTYPES_H
