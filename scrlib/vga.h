1cc1bf305866cb6a6eb115a63e7424bca8534e5a
NunoLava1998 <nunolava1998@gmail.com>
2017-02-24 17:27:01 +0000

    Adding first commit

---
+++ scrlib/vga.h
@@ -0,0 +1,22 @@
+
+#ifndef _VGA_H_
+#define _VGA_H_
+
+#define BLACK 0x00
+#define BLUE 0x01
+#define GREEN 0x02
+#define CYAN 0x03
+#define RED 0x04
+#define MAGENTA 0x05
+#define BROWN 0x06
+#define LIGHT_GRAY 0x07
+#define DARK_GRAY 0x08
+#define BRIGHT_BLUE 0x09
+#define BRIGHT_GREEN 0x0A
+#define BRIGHT_CYAN 0x0B
+#define BRIGHT_RED 0x0C
+#define BRIGHT_MAGENTA 0x0D
+#define BRIGHT_YELLOW 0x0E
+#define WHITE 0x0F
+
+#endif // _VGA_H_
