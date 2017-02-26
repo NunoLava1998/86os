f0834b5ef108b1e154b40353428b8ecb71a838d2
NunoLava1998 <nunolava1998@gmail.com>
2017-02-26 11:48:33 +0000

    Adding the GDT

--- prototypes.h
+++ prototypes.h
@@ -6,8 +6,12 @@ extern int x;
 extern int y;
 extern unsigned short int* vptr;
 extern void update_vptr(int, int);
-extern void update_coord(unsigned short int*);
+extern void scroll(void);
 extern void putc(char, unsigned char, unsigned char);
 extern void puts(const char*, unsigned char, unsigned char);
+extern void make_gdt_entry(int, unsigned long, unsigned long, unsigned char, unsigned char);
+extern void gdt_flush(void);
+extern void load_gdt(void);
+
 
 #endif // _PROTOTYPES_H
