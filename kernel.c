693c51d6f6c418cf75a3941634f88a951c7c6c4a
NunoLava1998 <nunolava1998@gmail.com>
2017-02-27 17:07:41 +0000

    Add files via upload

--- kernel.c
+++ kernel.c
@@ -3,5 +3,7 @@
 
 void kmain(void) {
 	load_gdt();
+	load_idt();
+	pic_start();
 	for(;;);
 }
--- prototypes.h
+++ prototypes.h
@@ -12,6 +12,17 @@ extern void puts(const char*, unsigned char, unsigned char);
 extern void make_gdt_entry(int, unsigned long, unsigned long, unsigned char, unsigned char);
 extern void gdt_flush(void);
 extern void load_gdt(void);
+extern int default_isr(void);
+extern int default_irq(void);
+extern void memset(void*, unsigned char, int);
+extern void lidt_core(unsigned long);
+extern void make_idt_entry(int, unsigned long, unsigned short, unsigned char);
+extern void load_idt(void);
+extern unsigned char inb(unsigned short);
+extern void outb(unsigned short, unsigned char);
+extern void hw_wait(void);
+extern void pic_initialize(void);
+extern void pic_start(void);
 
 
 #endif // _PROTOTYPES_H
